de623f2d338a7e2edae95c2958dfa0c27f1c3c83
# <#>
2025-08-19 19:46:20 +0800

    feat: 批处理-1

--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -3,6 +3,7 @@ import { Action } from 'shared/ReactTypes';
 
 export interface Update<State>{
     action: Action<State>; // 更新动作
+    next: Update<any> | null;
 }
 export interface UpdateQueue<State> {
     shared:{
@@ -24,8 +25,18 @@ export const createUpdateQueue = <State>() => {
         dispatch: null
     } as UpdateQueue<State>;
 }
-export const enqueueUpdate = <State>(updateQueue: UpdateQueue<State>, update: Update<State>) => {
-    updateQueue.shared.pending = update; // 将更新添加到待处理队列中
+export const enqueueUpdate = <State>(
+    updateQueue: UpdateQueue<State>, 
+    update: Update<State>
+) => {
+    const pending =  updateQueue.shared.pending;
+    if(pending === null){
+        update.next = update;
+    }else{
+        update.next = pending.next;
+        pending.next = update;
+    }
+    updateQueue.shared.pending = update;
 }
 export const processUpdateQueue = <State>(baseState: State, pendingUpdate: Update<State> | null):{memoizedState:State} => {
     const result:ReturnType<typeof processUpdateQueue<State>> = { memoizedState: baseState }; // 初始化结果为基础状态
